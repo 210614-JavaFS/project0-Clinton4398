098a4460a2f50f47f9b21afeeb5b53cb3b367e22
Clinton N. Obi <clintonn.obi@macusers-MacBook-Pro.local>
2021-06-28 17:57:14 -0400

    push test

--- src/main/java/com/revature/Placeholder.java
+++ src/main/java/com/revature/Placeholder.java
@@ -6,5 +6,7 @@ public interface Placeholder {
 	 * This is a placeholder file created simple to maintain the Maven file structure on GitHub. 
 	 * You may immediately delete this file from your project, you will not require it. 
 	 */
-
+//Just testing somethings out;
+	
+	public static int run = 6;
 }
